89b28c74903227209391acceb8ca78f0e7b2ba39
Joel Scott <jscottusf@gmail.com>
2020-07-03 15:42:10 -0400

    added some styling to register /login page

--- BrewGoSocial/ClientApp/src/app/account/account.module.ts
+++ BrewGoSocial/ClientApp/src/app/account/account.module.ts
@@ -1,22 +1,20 @@
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { JwtInterceptor, ErrorInterceptor } from "../_helpers";
 
-import { AccountRoutingModule } from './account-routing.module';
-import { LayoutComponent } from './layout.component';
-import { LoginComponent } from './login.component';
-import { RegisterComponent } from './register.component';
+import { AccountRoutingModule } from "./account-routing.module";
+import { LayoutComponent } from "./layout.component";
+import { LoginComponent } from "./login.component";
+import { RegisterComponent } from "./register.component";
 
 @NgModule({
-    imports: [
-        CommonModule,
-        ReactiveFormsModule,
-        AccountRoutingModule
-    ],
-    declarations: [
-        LayoutComponent,
-        LoginComponent,
-        RegisterComponent
-    ]
+  imports: [CommonModule, ReactiveFormsModule, AccountRoutingModule],
+  declarations: [LayoutComponent, LoginComponent, RegisterComponent],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
 })
-export class AccountModule { }
+export class AccountModule {}
