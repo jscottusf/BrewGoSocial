a9c541f8e6b0ffbd3e2fa0d6fa426f3dc10d596d
Joel Scott <jscottusf@gmail.com>
2020-07-06 15:30:05 -0400

    working on zomato API service

--- BrewGoSocial/ClientApp/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: "",
-  mapBoxAccessToken:
-    "pk.eyJ1IjoianNjb3R0dXNmIiwiYSI6ImNrNjFpbWEydjAxbjgzam9hZTgyd3hoN3QifQ.tUiC-b5WfvxUJuYp49Vqzw",
-};
