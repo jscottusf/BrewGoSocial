7e1cdc0de75a171cb1b8455f3d1515c76ddf80a6
Joel Scott <jscottusf@gmail.com>
2020-07-17 16:16:51 -0400

    working on post model / service / controller

---
+++ BrewGoSocial/Models/Post.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BrewGoSocial.Models.Users;
+
+namespace BrewGoSocial.Models
+{
+    public class Post : UserBase
+    {
+        [Key]
+        public int PostId { get; set; }
+        public string PostBody { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ BrewGoSocial/Models/Users/UserBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BrewGoSocial.Models.Users
+{
+    public class UserBase : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Slug { get; set; }
+    }
+}
