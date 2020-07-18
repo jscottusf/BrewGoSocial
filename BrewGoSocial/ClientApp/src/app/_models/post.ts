36ca41bb8ba79c6d5339a876598eee71bc96191d
Joel Scott <jscottusf@gmail.com>
2020-07-18 15:07:27 -0400

    added post model and service to angular app

---
+++ BrewGoSocial/ClientApp/src/app/_models/post.ts
@@ -0,0 +1,11 @@
+export class PostModel {
+  postId: number;
+  postBody: string;
+  userId: number;
+  firstName: string;
+  lastName: string;
+  username: string;
+  slug: string;
+  createdDate: string;
+  updatedDate: string;
+}
---
+++ BrewGoSocial/ClientApp/src/app/_services/post.service.ts
@@ -0,0 +1,40 @@
+import { PostModel } from "../_models/Post";
+import { HttpClient } from "@angular/common/http";
+import { Injectable, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "../../environments/environment";
+
+@Injectable({ providedIn: "root" })
+export class PostService implements OnInit {
+  appurl: string = "";
+
+  constructor(private http: HttpClient) {
+    this.appurl = `${environment.apiUrl}/api/posts/`;
+  }
+
+  ngOnInit() {}
+
+  getUserPosts(id): Observable<PostModel> {
+    return this.http.get<PostModel>(`${environment.apiUrl}/api/users/${id}`);
+  }
+
+  getallPosts(): Observable<PostModel> {
+    return this.http.get<PostModel>(this.appurl);
+  }
+
+  getPostById(id): Observable<PostModel> {
+    return this.http.get<PostModel>(this.appurl + id);
+  }
+
+  postNewPost(formData: PostModel) {
+    return this.http.post(this.appurl, formData);
+  }
+
+  deletePost(id: number) {
+    return this.http.delete(this.appurl + id);
+  }
+
+  editPost(id: number, formData: PostModel) {
+    return this.http.put(this.appurl + id, formData);
+  }
+}
