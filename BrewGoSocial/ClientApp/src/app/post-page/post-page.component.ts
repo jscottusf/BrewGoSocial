7dd78d15f63e663bc9f2e652d85a893b579825b8
Joel Scott <jscottusf@gmail.com>
2020-07-22 13:36:40 -0400

    added edit post modal for post /comments page

--- BrewGoSocial/ClientApp/src/app/post-page/post-page.component.ts
+++ BrewGoSocial/ClientApp/src/app/post-page/post-page.component.ts
@@ -18,6 +18,7 @@ export class PostPageComponent implements OnInit {
   postId: string;
   public comments: CommentModel[];
   public post: PostModel;
+  public editForm: FormGroup;
 
   constructor(
     private accountService: AccountService,
@@ -25,7 +26,8 @@ export class PostPageComponent implements OnInit {
     private commentService: CommentService,
     private modalService: NgbModal,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
   ) {
     this.user = this.accountService.userValue;
     this.postId = this.route.snapshot.paramMap.get("id");
@@ -42,12 +44,57 @@ export class PostPageComponent implements OnInit {
       .then((data) => {
         this.post = data;
         this.comments = data.comments;
-        console.log(this.comments);
+        this.setForm();
       })
       .catch((err) => console.log(err));
   }
 
   exexOnComment() {
     this.getPostData(this.postId);
+  }
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.editForm.controls;
+  }
+
+  setForm(editForm?: FormGroup) {
+    if (editForm != null) editForm.reset();
+    this.editForm = this.formBuilder.group({
+      postId: this.post.postId,
+      profileImgUrl: this.post.profileImgUrl,
+      postBody: [this.post.postBody, [Validators.minLength(1)]],
+      userId: this.post.userId,
+      firstName: this.post.firstName,
+      lastName: this.post.lastName,
+      username: this.post.username,
+      slug: this.post.slug,
+    });
+  }
+
+  submitChanges() {
+    this.submitted = true;
+    // stop here if form is invalid (only whitespace)
+    if (/\S/.test(this.editForm.value.postBody) === false) {
+      return;
+    }
+    console.log(this.editForm.value);
+    this.postService
+      .editPost(this.post.postId, this.editForm.value)
+      .subscribe((res) => {
+        this.getPostData(this.postId);
+      }),
+      (err) => console.log(err);
+  }
+
+  deletePost(id) {
+    this.postService.deletePost(id).subscribe(
+      (res) => this.getPostData(this.postId),
+      (err) => console.log(err)
+    );
+  }
+
+  open(content) {
+    this.modalService.open(content);
   }
 }
