61ebe4dd79eef1f3ea15e1cebda53d0a50098b67
Joel Scott <jscottusf@gmail.com>
2020-08-29 15:07:52 -0400

    adding like component to profile post. modified user controller to pull in likes via slug

--- BrewGoSocial/Services/UserService.cs
+++ BrewGoSocial/Services/UserService.cs
@@ -71,7 +71,13 @@ namespace BrewGoSocial.Services
 
         public User GetBySlug(string slug)
         {
-            return _context.Users.Include(x => x.Posts).ThenInclude(post => post.Comments).Include(x => x.Profile).Include(x => x.Follows).FirstOrDefault(u => u.Slug == slug);
+            return _context.Users.Include(x => x.Posts)
+                .ThenInclude(post => post.Comments)
+                .Include(x => x.Posts)
+                .ThenInclude(post => post.Likes)
+                .Include(x => x.Profile)
+                .Include(x => x.Follows)
+                .FirstOrDefault(u => u.Slug == slug);
         }
 
         public User Create(User user, string password)
