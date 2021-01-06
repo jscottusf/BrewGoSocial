25b2c639576afc21f7462b66e8d593891ff3d2d5
Joel Scott <jscottusf@gmail.com>
2021-01-05 22:59:43 -0500

    expansion card accordion fix

--- BrewGoSocial/Controllers/UsersController.cs
+++ BrewGoSocial/Controllers/UsersController.cs
@@ -169,7 +169,6 @@ namespace BrewGoSocial.Controllers
             return Ok(model);
         }
 
-
         [HttpPost("imgupload/{id}")]
         public async Task FileImageAsync(int id)
         {
